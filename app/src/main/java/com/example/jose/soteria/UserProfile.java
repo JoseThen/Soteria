224930d44c5da11ce94368b6c4623282b4cd075f
rappinkraken <thenj@ymail.com>
2015-11-19 11:17:53 -0500

    Added Needed SDK Files
    Getting the Project started!

---
+++ app/src/main/java/com/example/jose/soteria/UserProfile.java
@@ -0,0 +1,35 @@
+package com.example.jose.soteria;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);}
+        /*
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }*/
+        public void dooSomething(View v){
+            Button button = (Button) v;
+            startActivity(new Intent(getApplicationContext(), Panic.class));
+        }
+}
